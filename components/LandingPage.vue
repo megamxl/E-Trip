67e8c25e58b809eeb15e2e19f73ad3916215a491
SpodoY <72346498+SpodoY@users.noreply.github.com>
2022-05-06 21:53:19 +0200

    LandingPage.vue basic layout concept created - Work in progress

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="background">
-    <div class="left flex pillar">
-      <h1> Create your own profile to get the most out of E-Trip </h1>
-      <v-btn to="#" outlined rounded> Sign Up / Login </v-btn>
+    <div class="left d-flex flex-column justify-space-around pillar">
+      <v-container class="landingContainer rounded-lg">
+        <h1> Create your own profile to get the most out of E-Trip </h1>
+        <v-btn block x-large to="#" outlined rounded> Sign Up / Login </v-btn>
+      </v-container>
     </div>
-    <div class="right flex pillar">
-      <v-card>
+    <div class="right d-flex flex-column justify-space-around pillar">
+      <v-container class="landingContainer rounded-lg">
         <v-card-title> Look for an optimal route now </v-card-title>
         <v-text-field outlined label="From"> </v-text-field>
-        <v-text-field outlined label="To"> </v-text-field>
-      </v-card>
+        <v-container class="d-flex">
+          <v-text-field outlined label="To"> </v-text-field>
+          <v-btn > > </v-btn>
+        </v-container>
+      </v-container>
     </div>
   </div>
 
@@ -46,14 +51,12 @@ export default {
 .pillar {
   width: 30vw;
   height: 100vh;
-  background: rgba(0,0,0,0.7);
+  background: rgba(0, 0, 0, 0.7);
+  padding: 20px;
 }
 
-.flex {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+.landingContainer {
+  background: rgba(25, 25,25, 1);
 }
 
 
