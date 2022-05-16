c889f1c43159bf569de2a66bd0bdd2bb333399b1
Maxl <maximilian.nowak@outlook.at>
2022-05-16 22:36:00 +0200

    oopsie dayise i jut srew my self big time

--- pages/auth/Login.vue
+++ pages/auth/Login.vue
@@ -1,44 +1,36 @@
 <template>
-  <v-row align="center" justify="center">
-    <v-col cols="12" sm="8" md="4" align="center">
-      <v-card class="elevation-4 text-left" >
-        <v-card-title class="fancy-title align-center justify-center">E-trip</v-card-title><v-card-text>
-        <v-form>
-          <v-text-field
-            label="Login"
-            name="login"
-            prepend-icon="mdi-account"
-            type="text"
-            v-model="auth.email"
-          ></v-text-field>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="4">
+      <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="">
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          type="email"
+          label="E-mail"
+          autocomplete="username"
+          required
+        ></v-text-field>
 
-          <v-text-field
-            label="Password"
-            name="password"
-            prepend-icon="mdi-lock"
-            type="password"
-            v-model="auth.password"
-          ></v-text-field>
-        </v-form>
-      </v-card-text>
-        <v-card-actions class="text-center">
-          <v-btn
-            class="login-button"
-            @click="login"
-            depressed
-            large
-          >Login</v-btn
-          >
-        </v-card-actions>
-      </v-card>
-      <v-snackbar
-        :timeout="4000"
-        v-model="snackbar"
-        absolute
-        bottom
-        center
-      >
-        {{ snackbarText }}
+        <v-text-field
+          v-model="password"
+          :rules="passwordRules"
+          type="password"
+          label="Password"
+          autocomplete="current-password"
+          required
+        ></v-text-field>
+
+        <v-btn :disabled="!valid" color="success" class="mr-4" @click="login">
+          Login
+        </v-btn>
+
+        <v-btn color="success" class="mr-4" @click="forgetPassword">
+          Forget Password
+        </v-btn>
+      </v-form>
+
+      <v-snackbar v-model="snackbar" color="error">
+        {{ errorMessage }}
       </v-snackbar>
     </v-col>
   </v-row>
@@ -46,29 +38,60 @@
 
 <script>
 export default {
-  layout: 'signin',
-  data() {
-    return {
-      snackbar: false,
-      snackbarText: 'No error message',
-      auth: {
-        email: '',
-        password: ''
-      }
-    }
-  },
+  layout: "plain",
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+    ],
+    password: "",
+    passwordRules: [
+      (v) => !!v || "Password is required",
+      (v) => (v && v.length >= 6) || "Password must minimums 6 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    snackbar: false,
+    errorMessage: "",
+  }),
   methods: {
     login() {
-      let that = this
-      this.$fire.auth.signInWithEmailAndPassword(this.auth.email, this.auth.password)
-        .catch(function (error){
-          that.snackbarText = error.message
-          that.snackbar = true
-        }).then((user) => {
-        //we are signed in
-        $nuxt.$router.push('/RoutePage')
-      })
-    }
-  }
-}
+      let formValidation = this.$refs.form.validate();
+      if (formValidation) {
+        this.$fire.auth
+          .signInWithEmailAndPassword(this.email, this.password)
+          .then((userCredential) => {
+            const authUser = {
+              uid: userCredential.user.uid,
+              email: userCredential.user.email,
+            };
+            this.$store
+              .dispatch("onAuthStateChangedAction", {
+                authUser,
+              })
+              .then(() => {
+                this.$router.replace("/routepage");
+              })
+              .catch((error) => {
+                console.log("User State error", error);
+              });
+          })
+          .catch((error) => {
+            console.log("Login error", error);
+            this.snackbar = true;
+            this.errorMessage = error.message;
+          });
+      }
+    },
+    forgetPassword() {
+      console.log("clicked on forget password");
+      this.$router.push("/auth/resetpassword");
+    },
+  },
+};
 </script>
