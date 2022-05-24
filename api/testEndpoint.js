3d63d5fa62222dfc205b5d9904635661e9ce8476
Maxl <maximilian.nowak@outlook.at>
2022-05-24 11:53:48 +0200

    changed reqeusts

--- api/testEndpoint.js
+++ api/testEndpoint.js
@@ -5,82 +5,7 @@ const app = express();
 app.use(express.json());
 
 
-function test() {
-  const https = require('https');
-  require('dotenv').config();
-
-  const data = JSON.stringify({
-    query: `query carListSearch {
-  carList(search: "Tesla") {
-    id
-    naming {
-      chargetrip_version
-      edition
-      make
-      model
-      version
-    }
-    # add more fields here
-  }
-}`,
-  });
-
-  const options = {
-    hostname: 'https://api.chargetrip.io/graphql', //path: '/api/example/__graphql',
-     method: 'POST', headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length, //x-client-id'
-      'x-client-id': '62335e76c351300738296e30 ' + process.env.STEPZEN_API_KEY,
-      'User-Agent': 'Node',
-    },
-  };
-  const req = https.request(options, (res) => {
-    let data = '';
-    console.log(`statusCode: ${res.statusCode}`);
-
-    res.on('data', (d) => {
-      data += d;
-    });
-    res.on('end', () => {
-      console.log(JSON.parse(data).data);
-    });
-  });
-
-  req.on('error', (error) => {
-    console.error(error);
-  });
-
-  req.write(data);
-  req.end();
-  /*
-  const req = https.request(options, (res) => {
-    let data = '';
-    console.log(`statusCode: ${res.statusCode}`);
-
-    res.on('data', (d) => {
-      data += d;
-    });
-    res.on('end', () => {
-
-      console.log(JSON.parse(data).data);
-    });
-  });
-
-  req.on('error', (error) => {
-    console.error(error);
-  });
-
-
-
-  req.write(data);
-  req.end();
-
-   */
-
-}
-
-
-function seconTry(){
+function fetch(qeury){
   fetch('https://api.chargetrip.io/graphql', {
     method: 'POST',
     headers: {
@@ -88,26 +13,52 @@ function seconTry(){
       'Accept': 'application/json',
       'x-client-id': '62335e76c351300738296e30',
     },
-    body: JSON.stringify({query: 'query carListSearch {  carList(search: "Tesla") {    id    naming {     chargetrip_version      edition     make      model      version   }  }}',
+    //body: JSON.stringify({query: 'query carListSearch {  carList(search: "Tesla") {    id    naming {     chargetrip_version      edition     make      model      version   }  }}',
+    body: JSON.stringify({qeury,
       variables: { }})
   })
-    .then(r => r.json()).then(data => console.log('data returned:', data));
-
+    .then(r => r.json()).then(data => console.log(data) );
 }
 
 app.post('/get', async (req, res) => {
 
-  fetch('https://api.chargetrip.io/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'x-client-id': '62335e76c351300738296e30',
-    },
-    body: JSON.stringify({query: 'query carListSearch {  carList(search: "Tesla") {    id    naming {     chargetrip_version      edition     make      model      version   }  }}',
-      variables: { }})
-  })
-    .then(r => r.json()).then(data => res.send(data) );
+
+
+
+});
+
+
+
+
+app.post('/createRoute', async (req,res) =>{
+
+
+
+});
+
+
+app.get('/getRoute', async (req,res) =>{
+
+
+
+});
+
+app.get('/getChargerNearby', async (req,res) =>{
+
+
+
+});
+
+
+app.delete('/removeRoute' , async (req,res) =>{
+
+
+
+});
+
+
+app.put('/mutateRoute', async (req,res) =>{
+
 
 
 });
