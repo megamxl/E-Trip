54ac34cf0bd0fb74c0de3f812d9d662bb1af636b
Chronikle135 <Simon.Nenning@gmx.at>
2022-05-30 14:43:00 +0200

    Implemented Endpoints. carDataEnpoint.js and chargerEndpoint.js is still static. routeEndpoint.js needs bodys in the request

--- api/carDataEnpoint.js
+++ api/carDataEnpoint.js
@@ -1,35 +1,56 @@
 const express = require('express');
 const https = require("https");
+const {response} = require("express");
+import fetch from 'node-fetch';
+
 
 const app = express();
 app.use(express.json());
 
+const carListAll = 'query carListAll {\n'+
+  '  carList(size:407) {\n'+
+  '    id\n'+
+  '    connectors{standard}\n'+
+  '    naming{\n'+
+  '      model\n'+
+  '      make\n'+
+  '    }\n'+
+  '  }}';
 
-function fetch(qeury){
-  fetch('https://api.chargetrip.io/graphql', {
+const carID ='query car($carId: ID!) {\n'+
+  '  car(id: $carId) {\n'+
+  '    naming{\n'+
+  '      make\n'+
+  '    	model\n'+
+  '    }\n'+
+  '  }\n'+
+  '}';
+let ID = '"5d161be5c9eef46132d9d20a"'
+
+let carIDVariable = '{"carId":'+ID+'}';
+let carListVariables = null
+async function graphQLRequest(ourBody, ourVariables) {
+  return await fetch('https://api.chargetrip.io/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Accept': 'application/json',
       'x-client-id': '62335e76c351300738296e30',
     },
-    //body: JSON.stringify({query: 'query carListSearch {  carList(search: "Tesla") {    id    naming {     chargetrip_version      edition     make      model      version   }  }}',
-    body: JSON.stringify({qeury,
-      variables: { }})
-  })
-    .then(r => r.json()).then(data => console.log(data) );
+    body: JSON.stringify({
+      query: ourBody,
+      variables: ourVariables,
+    })
+  }).then(r => r.json())
 }
 
-app.get('/getAllCars', async (req,res) =>{
-
-
+app.get('/getAllCars', async (req, res) => {
+  res.send(await graphQLRequest(carListAll,carListVariables))
 });
 
-app.get('/getCarById', async (req,res) =>{
-
+app.get('/getCarById', async (req, res) => {
+  res.send(await graphQLRequest(carID,carIDVariable))
 
 });
-
 
 
 module.exports = app
--- api/chargerEndpoint.js
+++ api/chargerEndpoint.js
@@ -1,27 +1,44 @@
 const express = require('express');
 const https = require("https");
+import fetch from 'node-fetch';
 
 const app = express();
 app.use(express.json());
 
 
-function fetch(qeury){
-  fetch('https://api.chargetrip.io/graphql', {
+let chargingPoints ='query stationAround {\n'+
+  '  stationAround(\n'+
+  '    query: {\n'+
+  '      location: { type: Point, coordinates: [16.38, 48.15] }\n'+
+  '      distance: 10000\n'+
+  '      power: 22\n'+
+  '    }\n'+
+  '  ) \n'+
+  '  {name\n'+
+  '    address\n'+
+  '    postal_code\n'+
+  '    city\n'+
+  '    power\n'+
+  '    speed\n'+
+  '  }\n'+
+  '}';
+
+async function graphQLRequest(ourBody, ourVariables) {
+  return await fetch('https://api.chargetrip.io/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Accept': 'application/json',
       'x-client-id': '62335e76c351300738296e30',
     },
-    //body: JSON.stringify({query: 'query carListSearch {  carList(search: "Tesla") {    id    naming {     chargetrip_version      edition     make      model      version   }  }}',
-    body: JSON.stringify({qeury,
-      variables: { }})
-  })
-    .then(r => r.json()).then(data => console.log(data) );
+    body: JSON.stringify({
+      query: ourBody,
+      variables: {ourVariables},
+    })
+  }).then(r => r.json())
 }
 
 app.get('/getChargerNearby', async (req,res) =>{
-
+  res.send(await graphQLRequest(chargingPoints))
 
 
 });
