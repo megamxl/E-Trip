8e5cf8050501bafd03e006786ac276907aef2380
Chronikle135 <Simon.Nenning@gmx.at>
2022-06-16 19:50:22 +0200

    Implemented carByBrand and parsing to xml

--- api/chargerEndpoint.js
+++ api/chargerEndpoint.js
@@ -2,29 +2,32 @@ const express = require('express');
 const https = require("https");
 import fetch from 'node-fetch';
 
+const options = {compact: true, ignoreComment: true, spaces: 4}
+const xml = require('xml-js');
+
 const app = express();
 app.use(express.json());
 
-function chargingPoints(body){
-return 'query stationAround {\n'+
-  '  stationAround(\n'+
-  '    query: {\n'+
-  '      location: { type: Point, coordinates: ['+body.longitudeStart+', '+body.latitudeStart+'] }\n'+
-  '      distance: '+body.distance+'\n'+
-  '    }\n'+
-  '  ) \n'+
-  '  {name\n'+
-  '    address\n'+
-  '    postal_code\n'+
-  '    city\n'+
-  '    power\n'+
-  '    speed\n'+
-  '  }\n'+
-  '}';
+function chargingPoints(body) {
+  return 'query stationAround {\n' +
+    '  stationAround(\n' +
+    '    query: {\n' +
+    '      location: { type: Point, coordinates: [' + body.longitudeStart + ', ' + body.latitudeStart + '] }\n' +
+    '      distance: ' + body.distance + '\n' +
+    '    }\n' +
+    '  ) \n' +
+    '  {name\n' +
+    '    address\n' +
+    '    postal_code\n' +
+    '    city\n' +
+    '    power\n' +
+    '    speed\n' +
+    '  }\n' +
+    '}';
 }
 
-async function graphQLRequest(ourBody, ourVariables) {
-  return await fetch('https://api.chargetrip.io/graphql', {
+async function graphQLRequest(ourBody, xmlHeader, ourVariables) {
+  let answer = await fetch('https://api.chargetrip.io/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -32,15 +35,20 @@ async function graphQLRequest(ourBody, ourVariables) {
     },
     body: JSON.stringify({
       query: ourBody,
-      variables: {ourVariables},
+      variables: ourVariables,
     })
   }).then(r => r.json())
+  if (xmlHeader === "true") {
+    return xml.json2xml(answer, options)
+  } else {
+    return answer
+  }
 }
 
-function checkBodyForChargers(body){
-  if(typeof body.longitudeStart   === 'number' && !Number.isNaN(body.longitudeStart) && !Number.isInteger(body.longitudeStart)){
-    if(typeof body.latitudeStart   === 'number' && !Number.isNaN(body.latitudeStart) && !Number.isInteger(body.latitudeStart)  ){
-      if(typeof body.distance   === 'number' && !Number.isNaN(body.distance) && Number.isInteger(body.distance) ){
+function checkBodyForChargers(body) {
+  if (typeof body.longitudeStart === 'number' && !Number.isNaN(body.longitudeStart) && !Number.isInteger(body.longitudeStart)) {
+    if (typeof body.latitudeStart === 'number' && !Number.isNaN(body.latitudeStart) && !Number.isInteger(body.latitudeStart)) {
+      if (typeof body.distance === 'number' && !Number.isNaN(body.distance) && Number.isInteger(body.distance)) {
         return true
       }
     }
@@ -50,11 +58,10 @@ function checkBodyForChargers(body){
 }
 
 
-app.get('/getChargerNearby', async (req,res) =>{
-  if(checkBodyForChargers(req.body)){
-
-    res.send(await graphQLRequest(chargingPoints(req.body)))
-  }else{
+app.post('/getChargerNearby', async (req, res) => {
+  if (checkBodyForChargers(req.body)) {
+    res.send(await graphQLRequest(chargingPoints(req.body), req.headers.xml))
+  } else {
     res.sendStatus(400)
   }
 
--- api/routeEndpoint.js
+++ api/routeEndpoint.js
@@ -1,141 +1,143 @@
-import {parse} from "graphql";
-
 const express = require('express');
 const https = require("https");
 import fetch from 'node-fetch';
+const options = {compact: true, ignoreComment: true, spaces: 4}
+const xml = require('xml-js');
 
 const app = express();
 app.use(express.json());
 
-function routeId(body){
-return 'mutation newRoute($carId: ID!) {\n'+
-  '  newRoute(\n'+
-  '    input: {\n'+
-  '      ev: { id: $carId, battery: { stateOfCharge: { value: '+body.chargeValue+', type: '+body.chargeValueType+' } }, climate: true, occupants: '+body.occupants+' }\n'+
-  '      routeRequest: {\n'+
-  '        origin: {\n'+
-  '          type: Feature\n'+
-  '          geometry: { type: Point, coordinates: ['+body.longitudeStart+', '+body.latitudeStart+'] }\n'+
-  '          properties: { name: "'+body.cityStart+', '+body.countryStart+'" }\n'+
-  '        }\n'+
-  '        destination: {\n'+
-  '          type: Feature\n'+
-  '          geometry: { type: Point, coordinates: ['+body.longitudeEnd+', '+body.latitudeEnd+'] }\n'+
-  '          properties: { name: "'+body.cityEnd+', '+body.countryEnd+'" }\n'+
-  '        }\n'+
-  '      }\n'+
-  '    }\n'+
-  '  )\n'+
-  '}';
-}
-function planRoute(routeID){
-return 'query getRoute {\n'+
-  '  route(id: "'+ routeID+'") {\n'+
-  '    route {\n'+
-  '      id\n'+
-  '      type\n'+
-  '      charges\n'+
-  '      distance\n'+
-  '      duration\n'+
-  '      consumption\n'+
-  '      chargeTime\n'+
-  '      amenityRanking\n'+
-  '      rangeStart\n'+
-  '      rangeStartKwh\n'+
-  '      rangeEnd\n'+
-  '      rangeEndKwh\n'+
-  '      via\n'+
-  '      elevationUp\n'+
-  '      elevationDown\n'+
-  '      elevationMax\n'+
-  '      pathPlot{\n'+
-  '        elevation\n'+
-  '        consumptionPerKm\n'+
-  '        averageSpeed\n'+
-  '      }\n'+
-  '      polyline\n'+
-  '      saving {\n'+
-  '        co2\n'+
-  '        money\n'+
-  '        currency\n'+
-  '        averageGasPrice\n'+
-  '        averageEnergyPrice\n'+
-  '      }\n'+
-  '      legs {\n'+
-  '        id\n'+
-  '        distance\n'+
-  '        duration\n'+
-  '        consumption\n'+
-  '        rangeStart\n'+
-  '        rangeStartKwh\n'+
-  '        rangeEnd\n'+
-  '        rangeEndKwh\n'+
-  '        origin {\n'+
-  '          id\n'+
-  '          type\n'+
-  '          geometry {\n'+
-  '            type\n'+
-  '            coordinates\n'+
-  '          }\n'+
-  '          properties\n'+
-  '        }\n'+
-  '        destination {\n'+
-  '          id\n'+
-  '          type\n'+
-  '          geometry {\n'+
-  '            type\n'+
-  '            coordinates\n'+
-  '          }\n'+
-  '          properties\n'+
-  '        }\n'+
-  '        type\n'+
-  '        name\n'+
-  '        stationId\n'+
-  '        operatorId\n'+
-  '        chargeTime\n'+
-  '        evse {\n'+
-  '          uid\n'+
-  '          evse_id\n'+
-  '          physical_reference\n'+
-  '          status\n'+
-  '          connectors {\n'+
-  '            id\n'+
-  '            power\n'+
-  '            max_amperage\n'+
-  '            max_voltage\n'+
-  '            max_electric_power\n'+
-  '            standard\n'+
-  '            format\n'+
-  '            power_type\n'+
-  '            properties\n'+
-  '          }\n'+
-  '          parking_restrictions\n'+
-  '          properties\n'+
-  '        }\n'+
-  '        connector {\n'+
-  '          id\n'+
-  '          power\n'+
-  '          max_amperage\n'+
-  '          max_voltage\n'+
-  '          max_electric_power\n'+
-  '          standard\n'+
-  '          properties\n'+
-  '        }\n'+
-  '        plugsAvailable\n'+
-  '        plugsCount\n'+
-  '      }\n'+
-  '    }\n'+
-  '    status\n'+
-  '  }\n'+
-  '}\n'+
-  '';
-}
-function carId(carID){
-  return '{"carId":"'+carID+'"}';
+function routeId(body) {
+  return 'mutation newRoute($carId: ID!) {\n' +
+    '  newRoute(\n' +
+    '    input: {\n' +
+    '      ev: { id: $carId, battery: { stateOfCharge: { value: ' + body.chargeValue + ', type: ' + body.chargeValueType + ' } }, climate: true, occupants: ' + body.occupants + ' }\n' +
+    '      routeRequest: {\n' +
+    '        origin: {\n' +
+    '          type: Feature\n' +
+    '          geometry: { type: Point, coordinates: [' + body.longitudeStart + ', ' + body.latitudeStart + '] }\n' +
+    '          properties: { name: "' + body.cityStart + ', ' + body.countryStart + '" }\n' +
+    '        }\n' +
+    '        destination: {\n' +
+    '          type: Feature\n' +
+    '          geometry: { type: Point, coordinates: [' + body.longitudeEnd + ', ' + body.latitudeEnd + '] }\n' +
+    '          properties: { name: "' + body.cityEnd + ', ' + body.countryEnd + '" }\n' +
+    '        }\n' +
+    '      }\n' +
+    '    }\n' +
+    '  )\n' +
+    '}';
 }
 
-async function graphQLRequest(ourBody, ourVariables) {
-  return await fetch('https://api.chargetrip.io/graphql', {
+function planRoute(routeID) {
+  return 'query getRoute {\n' +
+    '  route(id: "' + routeID + '") {\n' +
+    '    route {\n' +
+    '      id\n' +
+    '      type\n' +
+    '      charges\n' +
+    '      distance\n' +
+    '      duration\n' +
+    '      consumption\n' +
+    '      chargeTime\n' +
+    '      amenityRanking\n' +
+    '      rangeStart\n' +
+    '      rangeStartKwh\n' +
+    '      rangeEnd\n' +
+    '      rangeEndKwh\n' +
+    '      via\n' +
+    '      elevationUp\n' +
+    '      elevationDown\n' +
+    '      elevationMax\n' +
+    '      pathPlot{\n' +
+    '        elevation\n' +
+    '        consumptionPerKm\n' +
+    '        averageSpeed\n' +
+    '      }\n' +
+    '      polyline\n' +
+    '      saving {\n' +
+    '        co2\n' +
+    '        money\n' +
+    '        currency\n' +
+    '        averageGasPrice\n' +
+    '        averageEnergyPrice\n' +
+    '      }\n' +
+    '      legs {\n' +
+    '        id\n' +
+    '        distance\n' +
+    '        duration\n' +
+    '        consumption\n' +
+    '        rangeStart\n' +
+    '        rangeStartKwh\n' +
+    '        rangeEnd\n' +
+    '        rangeEndKwh\n' +
+    '        origin {\n' +
+    '          id\n' +
+    '          type\n' +
+    '          geometry {\n' +
+    '            type\n' +
+    '            coordinates\n' +
+    '          }\n' +
+    '          properties\n' +
+    '        }\n' +
+    '        destination {\n' +
+    '          id\n' +
+    '          type\n' +
+    '          geometry {\n' +
+    '            type\n' +
+    '            coordinates\n' +
+    '          }\n' +
+    '          properties\n' +
+    '        }\n' +
+    '        type\n' +
+    '        name\n' +
+    '        stationId\n' +
+    '        operatorId\n' +
+    '        chargeTime\n' +
+    '        evse {\n' +
+    '          uid\n' +
+    '          evse_id\n' +
+    '          physical_reference\n' +
+    '          status\n' +
+    '          connectors {\n' +
+    '            id\n' +
+    '            power\n' +
+    '            max_amperage\n' +
+    '            max_voltage\n' +
+    '            max_electric_power\n' +
+    '            standard\n' +
+    '            format\n' +
+    '            power_type\n' +
+    '            properties\n' +
+    '          }\n' +
+    '          parking_restrictions\n' +
+    '          properties\n' +
+    '        }\n' +
+    '        connector {\n' +
+    '          id\n' +
+    '          power\n' +
+    '          max_amperage\n' +
+    '          max_voltage\n' +
+    '          max_electric_power\n' +
+    '          standard\n' +
+    '          properties\n' +
+    '        }\n' +
+    '        plugsAvailable\n' +
+    '        plugsCount\n' +
+    '      }\n' +
+    '    }\n' +
+    '    status\n' +
+    '  }\n' +
+    '}\n' +
+    '';
+}
+
+function carId(carID) {
+  return '{"carId":"' + carID + '"}';
+}
+
+async function graphQLRequest(ourBody, xmlHeader, ourVariables) {
+  let answer = await fetch('https://api.chargetrip.io/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -146,26 +148,30 @@ async function graphQLRequest(ourBody, ourVariables) {
       variables: ourVariables,
     })
   }).then(r => r.json())
+  if (xmlHeader === "true") {
+    return xml.json2xml(answer, options)
+  } else {
+    return answer
+  }
 }
 
-app.get('/getRoute', async (req,res) =>{
-  res.send(await graphQLRequest(planRoute(req.headers.routeid),carId(req.headers.carid)))
+app.get('/getRoute', async (req, res) => {
+  res.send(await graphQLRequest(planRoute(req.headers.routeid), req.headers.xml, carId(req.headers.carid)))
 });
 
 
-app.post('/createRoute', async (req,res) =>{
-  res.send(await graphQLRequest(routeId(req.body),carId(req.body.carID)))
-
-});
-
-
-app.delete('/removeRoute' , async (req,res) =>{
+app.post('/createRoute', async (req, res) => {
+  res.send(await graphQLRequest(routeId(req.body), req.headers.xml, carId(req.body.carID)))
 
 });
 
 
-app.put('/mutateRoute', async (req,res) =>{
+app.delete('/removeRoute', async (req, res) => {
 
+});
+
+
+app.put('/mutateRoute', async (req, res) => {
 
 
 });
